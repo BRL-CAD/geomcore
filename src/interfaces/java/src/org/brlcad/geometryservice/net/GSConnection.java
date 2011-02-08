43c7e0813a914bd233da2feedfd74c4d131266ab
David Loman <dloman77@gmail.com>
2011-02-08 20:43:43 +0000

    Import cleanup.

--- src/interfaces/java/src/org/brlcad/geometryservice/net/GSConnection.java
+++ src/interfaces/java/src/org/brlcad/geometryservice/net/GSConnection.java
@@ -1,9 +1,7 @@
 package org.brlcad.geometryservice.net;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.ByteBuffer;
