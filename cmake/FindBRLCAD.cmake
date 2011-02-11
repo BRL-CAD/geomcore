fb8b797a4d3753a598e39550bc623112277acf6c
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-02-11 18:00:55 +0000

    More tweaking of FindBRLCAD

--- cmake/FindBRLCAD.cmake
+++ cmake/FindBRLCAD.cmake
@@ -49,7 +49,15 @@
 #     BRLCAD_RENDER_LIBRARY - librender
 #     BRLCAD_TIE_LIBRARY - libtie
 #     BRLCAD_SYSV_LIBRARY - libsysv
-#     BRLCAD_TERMIO_LIBRARY - libtermio
+#     BRLCAD_TERMIO_LIBRARY - libtermio (non WIN32 systems)
+#
+#  Technically these are external but we need the versions
+#  tweaked for BRL-CAD.  If a developer wishes to use the
+#  BRL-CAD altered versions of these libraries to satisfy a
+#  "generic" request for the library, they'll need to assign
+#  the results of these variables to the non-BRL-CAD specific 
+#  variables they are using
+#
 #     BRLCAD_EXPPP_LIBRARY - SCL libexppp library
 #     BRLCAD_EXPRESS_LIBRARY - SCL libexpress library
 #     BRLCAD_STEPCORE_LIBRARY - SCL core library
@@ -57,13 +65,17 @@
 #     BRLCAD_STEPEDITOR_LIBRARY - SCL editor library
 #     BRLCAD_STEPUTILS_LIBRARY - SCL utils library
 #     BRLCAD_OPENNURBS_LIBRARY - openNURBS library
-#     BRLCAD_UTAHRLE_LIBRARY - libutahrle
 #
 #  In addition to the above variables, which are essentially unique
 #  to BRL-CAD, this routine will look for local copies of libraries
-#  installed with BRL-CAD and return  their results as would a 
+#  installed with BRL-CAD and return their results as would a 
 #  standard find_package for that library (if one exists).  The 
-#  libraries searched for:
+#  distinction between these libraries and those above is that these
+#  libraries do not have known modifications required by BRL-CAD that
+#  preclude system libraries from substituting for them, although
+#  if BRL-CAD was compiled against local versions of these it is
+#  not guaranteed (or in some cases expected) that a found system
+#  version will successfully replace the local copy.
 #
 #  libregex
 #  libpng
@@ -72,10 +84,8 @@
 #  tcl/tk
 #  incrTcl
 #  Togl
+#  utahrle
 # 
-#	$Revision:  $
-#	$Author:  $
-#
 #########################################################################
 
 SET(BRLCAD_ROOT "$ENV{BRLCAD_ROOT}")
@@ -88,82 +98,63 @@ ELSE(BRLCAD_BASE_DIR AND BRLCAD_ROOT)
 ENDIF(BRLCAD_BASE_DIR AND BRLCAD_ROOT)
 
 #First, find the install directories.
-IF(BRLCAD_BASE_DIR)
-    #if BRLCAD_BASE_DIR is set, then this makes it easy!
-    SET(BRLCAD_BIN_DIR "${BRLCAD_BASE_DIR}/bin")
-    SET(BRLCAD_INC_DIRS "${BRLCAD_BASE_DIR}/include" "${BRLCAD_BASE_DIR}/include/brlcad" "${BRLCAD_BASE_DIR}/include/openNURBS")
-    SET(BRLCAD_LIB_DIR "${BRLCAD_BASE_DIR}/lib")
-ELSE(BRLCAD_BASE_DIR)
-	 #try looking for BRL-CAD's brlcad-config - it should give us
-	 #a location
-    FIND_PATH(BRLCAD_BIN_DIR brlcad-config)
-	 IF(BRLCAD_BIN_PATH)
-		 EXEC_PROGRAM(${BRLCAD_BIN_DIR} ARGS --prefix OUTPUT_VARIABLE BRLCAD_BASE_DIR)
-	 ENDIF(BRLCAD_BIN_PATH)
-	 IF(NOT BRLCAD_BASE_DIR)
-		 SET(BRLCAD_HEADERS_DIR_CANDIDATES 
-			 /usr/brlcad/include/brlcad 
-			 /usr/local/brlcad/include/brlcad
-			 )
-		 #Look for headers if brlcad-config fails
-		 FIND_PATH(BRLCAD_HEADERS_DIR NAMES bu.h bn.h rt.h PATHS ${BRLCAD_HEADERS_DIR_CANDIDATES})
-		 IF(BRLCAD_HEADERS_DIR)
-			 GET_FILENAME_COMPONENT(BRLCAD_BASE_DIR ${BRLCAD_HEADERS_DIR} PATH)
-		 ENDIF(BRLCAD_HEADERS_DIR)
-	 ENDIF(NOT BRLCAD_BASE_DIR)
+IF(NOT BRLCAD_BASE_DIR)
+	#try looking for BRL-CAD's brlcad-config - it should give us
+	#a location for bin, and the parent will be the root dir
+	IF(NOT BRLCAD_BASE_DIR)
+		FIND_PROGRAM(BRLCAD_CONFIGEXE brlcad-config)
+		IF(BRLCAD_CONFIGEXE)
+			EXEC_PROGRAM(brlcad-config ARGS --prefix OUTPUT_VARIABLE BRLCAD_BASE_DIR)
+		ENDIF(BRLCAD_CONFIGEXE)
+	ENDIF(NOT BRLCAD_BASE_DIR)
 
-	 IF(NOT BRLCAD_BASE_DIR)
-		 MESSAGE(FATAL_ERROR "\nCould not find BRL-CAD root directory - please set BRLCAD_BASE_DIR in CMake")
-	 ENDIF(NOT BRLCAD_BASE_DIR)
+	#if that didn't work, see if we can find brlcad-config and use the parent path
+	IF(NOT BRLCAD_BASE_DIR)
+		FIND_PATH(BRLCAD_BIN_DIR brlcad-config)
+		IF(BRLCAD_BIN_DIR)
+			GET_FILENAME_COMPONENT(BRLCAD_BASE_DIR ${BRLCAD_BIN_DIR} PATH)
+		ENDIF(BRLCAD_BIN_DIR)
+	ENDIF(NOT BRLCAD_BASE_DIR)
 
-    #Find include directories (aka more than one)
-    SET(HEADERS_TO_SEARCH_FOR brlcad/bu.h bu.h opennurbs.h )
-   
-	 SET(INCLUDE_PATH_LIST "$ENV{PATH}")
-	 STRING(REGEX REPLACE "/bin:" "/include:" INCLUDE_PATH_LIST "${INCLUDE_PATH_LIST}")
-	 STRING(REGEX REPLACE "/bin$" "/include" INCLUDE_PATH_LIST "${INCLUDE_PATH_LIST}")	
-	 STRING(REGEX REPLACE ":" ";" INCLUDE_PATH_LIST "${INCLUDE_PATH_LIST}")	 
+	#Look for headers if we come up empty with brlcad-config
+	IF(NOT BRLCAD_BASE_DIR)
+		SET(BRLCAD_HEADERS_DIR_CANDIDATES 
+			/usr/brlcad/include/brlcad 
+			/usr/local/brlcad/include/brlcad
+			)
+		FIND_PATH(BRLCAD_HEADERS_DIR NAMES bu.h bn.h rt.h PATHS ${BRLCAD_HEADERS_DIR_CANDIDATES})
+		IF(BRLCAD_HEADERS_DIR)
+			GET_FILENAME_COMPONENT(BRLCAD_BASE_DIR ${BRLCAD_HEADERS_DIR} PATH)
+		ENDIF(BRLCAD_HEADERS_DIR)
+	ENDIF(NOT BRLCAD_BASE_DIR)
 
-    FOREACH (tHead ${HEADERS_TO_SEARCH_FOR})
-            
-		 FIND_PATH(_HEADER_DIR_${tHead} ${tHead} ${INCLUDE_PATH_LIST})
-          
-        IF(_HEADER_DIR_${tHead})
-			  SET(BRLCAD_INC_DIRS ${BRLCAD_INC_DIRS} ${_HEADER_DIR_${tHead}} ${_HEADER_DIR_${tHead}}/brlcad ${_HEADER_DIR_${tHead}}/openNURBS)
-            SET(BRLCAD_HEADERS_FOUND ${BRLCAD_HEADERS_FOUND} ${tHead})        
-        ELSE(_HEADER_DIR_${tHead})
-            SET(BRLCAD_HEADERS_NOTFOUND ${BRLCAD_HEADERS_NOTFOUND} ${tHead})        
-        ENDIF(_HEADER_DIR_${tHead})
+	IF(NOT BRLCAD_BASE_DIR)
+		MESSAGE(FATAL_ERROR "\nCould not find BRL-CAD root directory - please set BRLCAD_BASE_DIR in CMake")
+	ENDIF(NOT BRLCAD_BASE_DIR)
+ENDIF(NOT BRLCAD_BASE_DIR)
 
-    ENDFOREACH (tHead ${HEADERS_TO_SEARCH_FOR})
-    
-    IF(NOT BRLCAD_INC_DIRS)
-		 MESSAGE(STATUS "\t\tCould not find BRLCAD include directories anywhere in paths: ${INCLUDE_PATH_LIST}")
-    	RETURN()
-    ENDIF(NOT BRLCAD_INC_DIRS)
- 
-	 SET(LIB_PATH_LIST "$ENV{PATH}")
-	 STRING(REGEX REPLACE "/bin:" "/lib:" LIB_PATH_LIST "${LIB_PATH_LIST}")	
-	 STRING(REGEX REPLACE "/bin$" "/lib" LIB_PATH_LIST "${LIB_PATH_LIST}")	
-	 STRING(REGEX REPLACE ":" ";" LIB_PATH_LIST "${LIB_PATH_LIST}")	 
-        
-	 FIND_PATH(BRLCAD_LIB_DIR "libbu${CMAKE_SHARED_LIBRARY_SUFFIX}" ${LIB_PATH_LIST})
-    
-    IF(NOT BRLCAD_LIB_DIR)
-		 MESSAGE(STATUS "\t\tCould not find brlcad library directory in: ${LIB_PATH_LIST}")
-    	RETURN()
-    ENDIF(NOT BRLCAD_LIB_DIR)
+#Find include directories
+IF(NOT BRLCAD_HEADERS_DIR)
+	FIND_PATH(BRLCAD_HEADERS_DIR NAMES bu.h HINTS ${BRLCAD_BASE_DIR} PATH_SUFFIXES include/brlcad)
+ENDIF(NOT BRLCAD_HEADERS_DIR)
+FIND_PATH(BRLCAD_OPENNURBS_HEADERS_DIR NAMES opennurbs.h HINTS ${BRLCAD_BASE_DIR} PATH_SUFFIXES include/openNURBS include/opennurbs)
+SET(BRLCAD_INC_DIRS ${BRLCAD_HEADERS_DIR} ${BRLCAD_OPENNURBS_HEADERS_DIR})
 
-ENDIF(BRLCAD_BASE_DIR)
+#Find library directory
+FIND_PATH(BRLCAD_LIB_DIR "libbu${CMAKE_SHARED_LIBRARY_SUFFIX}" PATHS ${BRLCAD_BASE_DIR} PATH_SUFFIXES lib libs)
 
-
+#Find binary directory
+IF(NOT BRLCAD_BIN_DIR)
+	FIND_PATH(BRLCAD_BIN_DIR brlcad-config PATHS ${BRLCAD_BASE_DIR} PATH_SUFFIXES bin)
+ENDIF(NOT BRLCAD_BIN_DIR)
 
 #Attempt to get brlcad version.
-FIND_PROGRAM(BRLCAD_CONFIGEXE brlcad-config)
+IF(NOT BRLCAD_CONFIGEXE)
+	FIND_PROGRAM(BRLCAD_CONFIGEXE brlcad-config)
+ENDIF(NOT BRLCAD_CONFIGEXE)
 IF(BRLCAD_CONFIGEXE)
 	EXECUTE_PROCESS(COMMAND ${BRLCAD_CONFIGEXE} --version OUTPUT_VARIABLE BRLCAD_VERSION)
 	STRING(STRIP ${BRLCAD_VERSION} BRLCAD_VERSION)
-	
 	IF(BRLCAD_VERSION)
     	STRING(REGEX REPLACE "([0-9]+)\\.[0-9]+\\.[0-9]+" "\\1" BRLCAD_MAJOR_VERSION "${BRLCAD_VERSION}")
     	STRING(REGEX REPLACE "[0-9]+\\.([0-9]+)\\.[0-9]+" "\\1" BRLCAD_MINOR_VERSION "${BRLCAD_VERSION}")
@@ -173,10 +164,9 @@ IF(BRLCAD_CONFIGEXE)
         MESSAGE(STATUS "\t\t'brlcad-config --version' was found and executed, but produced no output.")
         SET(BRLCAD_VERSION_FOUND FALSE)
     ENDIF(BRLCAD_VERSION)
-
 ELSE(BRLCAD_CONFIGEXE)
-    MESSAGE(STATUS "\t\tCould not locate 'brlcad-config'.")
-    SET(BRLCAD_VERSION_FOUND FALSE)
+	MESSAGE(STATUS "Could not locate 'brlcad-config' - no BRL-CAD version available")
+	SET(BRLCAD_VERSION_FOUND FALSE)
 ENDIF(BRLCAD_CONFIGEXE)
 
 #TODO need to make the BRLCAD version checking a requirement for coreInterface, but nothing else.
