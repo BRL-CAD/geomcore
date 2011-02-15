ef4d5f72996968277b34862eb57f6656f08c6871
Erik Greenwald <erik@brlcad.org>
2011-02-15 19:02:20 +0000

    add GSUuid wrapper

---
+++ include/GSUuid.h
@@ -0,0 +1,67 @@
+/*                      G S U U I D . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file GSUuid.h
+ *
+ * Brief description
+ *
+ */
+
+#ifndef __GSUUID_H__
+#define __GSUUID_H__
+
+#include <string>
+
+#ifndef _UUID_T
+# include <uuid.h>
+#endif
+
+class GSUuid
+{
+public:
+  GSUuid();
+  GSUuid(GSUuid*);
+  GSUuid(std::string*);
+  ~GSUuid();
+
+  static GSUuid *createUuid();
+
+  bool equals(GSUuid*);
+
+  std::string toString();
+
+private:
+#if defined(uuid_s_ok)
+  struct uuid uuid;
+#elif defined(UUID_VARIANT_DCE)
+  uuid_t uuid;
+#endif
+};
+
+#endif /* __GSUUID_H__ */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
---
+++ src/utility/GSUuid.cxx
@@ -0,0 +1,69 @@
+/*                    G S U U I D . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file GSUuid.cxx
+ *
+ * cheezy uuid wrapper
+ *
+ */
+
+#include "GSUuid.h"
+
+GSUuid::GSUuid()
+{
+}
+
+GSUuid::GSUuid(GSUuid* src)
+{
+}
+
+GSUuid::GSUuid(std::string* str)
+{
+}
+
+GSUuid::~GSUuid()
+{
+}
+
+std::string
+GSUuid::toString()
+{
+}
+
+GSUuid *
+GSUuid::createUuid()
+{
+	return NULL;
+}
+
+bool
+GSUuid::equals(GSUuid* other)
+{
+	return false;
+}
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
