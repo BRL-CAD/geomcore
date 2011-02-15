9b411edbfdd8e1566f747dee50dd09aa4a903caf
David Loman <dloman77@gmail.com>
2011-02-15 15:54:45 +0000

    Implement minimal client's ability to login to a gs

--- src/interfaces/java/src/org/brlcad/geometryservice/minimalclient/cmd/LoginCmd.java
+++ src/interfaces/java/src/org/brlcad/geometryservice/minimalclient/cmd/LoginCmd.java
@@ -22,7 +22,11 @@
  */
 package org.brlcad.geometryservice.minimalclient.cmd;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.brlcad.geometryservice.GSJavaInterface;
+import org.brlcad.geometryservice.GSStatics;
 import org.brlcad.geometryservice.minimalclient.CmdConsolePanel;
 
 /**
@@ -48,15 +52,72 @@ public class LoginCmd extends AbstractCmd {
 	 */
 	@Override
 	public boolean doCmd(String[] args, GSJavaInterface gsji) {
-		
+
 		if (args.length < 4 || args.length > 5) {
 			this.printUsage();
 			return false;
 		}
 
-		this.cmdConsole.printToConsole("Not Implemented yet.", CmdConsolePanel.STYLE_BLUE_BOLD);
+		/* validate Args */
+		
+		// args[0] == cmd name
 
-		return true;
+		String uname = args[1];
+		if (uname.length() < 1) {
+			GSStatics.stdErr.println("Username too short.");
+			this.printUsage();
+			return false;
+		}
+
+		String passwd = args[2];
+		if (passwd.length() < 1) {
+			GSStatics.stdErr.println("Password too short.");
+			this.printUsage();
+			return false;
+		}
+
+		String addyStr = args[3];
+		if (addyStr.length() < 1) {
+			GSStatics.stdErr.println("Address too short.");
+			this.printUsage();
+			return false;
+		}
+
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName(addyStr);
+
+		} catch (UnknownHostException ehu) {
+			GSStatics.stdErr.println("Unknown Host: " + addyStr);
+			return false;
+
+		} catch (SecurityException se) {
+			GSStatics.stdErr.println("Security Exception for Host: " + addyStr);
+			return false;
+
+		} catch (Exception e) {
+			GSStatics.stdErr.println("Unexpected Exception thrown for Host: " + addyStr);
+			return false;
+		}
+
+		short port = 0;
+		if (args.length == 4) {
+			port = 5309;
+		} else {
+			try {
+				port = Short.parseShort(args[4]);
+			} catch (NumberFormatException nfe) {
+				GSStatics.stdErr.println("Connect attempt failed.  Bad Port number.");
+				return false;
+			} catch (Exception e) {
+				GSStatics.stdErr.println(e.getMessage());
+				GSStatics.stdErr.println("Connect attempt failed.");
+				return false;
+			}
+		}
+
+		/* Try connection */
+		return gsji.connectToHost(address, port, uname, passwd);	
 	}
 
 	/*
