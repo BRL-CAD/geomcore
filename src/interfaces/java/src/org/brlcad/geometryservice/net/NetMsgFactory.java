0325c06f906d0d8fbe21c1149d13ab7467d9fc9d
David Loman <dloman77@gmail.com>
2011-03-21 16:09:33 +0000

    Fill out the NetMsgFactory's switching table so that NetMsg's can actually be deserialized

--- src/interfaces/java/src/org/brlcad/geometryservice/net/NetMsgFactory.java
+++ src/interfaces/java/src/org/brlcad/geometryservice/net/NetMsgFactory.java
@@ -24,46 +24,54 @@ package org.brlcad.geometryservice.net;
 
 import org.brlcad.geometryservice.net.msg.AbstractNetMsg;
 import org.brlcad.geometryservice.net.msg.NetMsgTypes;
+import org.brlcad.geometryservice.net.msg.NewSessionReqMsg;
+import org.brlcad.geometryservice.net.msg.RemoteNodeNameSetMsg;
+import org.brlcad.geometryservice.net.msg.SessionInfoMsg;
 
 public final class NetMsgFactory {
 
 	public static AbstractNetMsg makeMsg(short type, ByteBufferReader reader) {
+		try {
+			switch (type) {
+			case NetMsgTypes.RUAlive:
+				return null;
+			case NetMsgTypes.IMAlive:
+				return null;
+			case NetMsgTypes.Failure:
+				return null;
+			case NetMsgTypes.Success:
+				return null;
+			case NetMsgTypes.Ping:
+				return null;
+			case NetMsgTypes.Pong:
+				return null;
+			case NetMsgTypes.RemNodeNameSET:
+				return new RemoteNodeNameSetMsg(reader);
+			case NetMsgTypes.DisconnectREQ:
+				return null;
+			case NetMsgTypes.NewNodeOnNet:
+				return null;
+			case NetMsgTypes.FullNodenameListREQ:
+				return null;
+			case NetMsgTypes.FullNodenameList:
+				return null;
+			case NetMsgTypes.NewSessionREQ:
+				return new NewSessionReqMsg(reader);
+			case NetMsgTypes.SessionInfo:
+				return new SessionInfoMsg(reader);
+			case NetMsgTypes.GeometryREQ:
+				return null;
+			case NetMsgTypes.GeometryMANIFEST:
+				return null;
+			case NetMsgTypes.GeometryCHUNK:
+				return null;
 
-		switch (type) {
-		case NetMsgTypes.RUAlive:
-			return null;
-		case NetMsgTypes.IMAlive:
-			return null;
-		case NetMsgTypes.Failure:
-			return null;
-		case NetMsgTypes.Success:
-			return null;
-		case NetMsgTypes.Ping:
-			return null;
-		case NetMsgTypes.Pong:
-			return null;
-		case NetMsgTypes.RemNodeNameSET:
-			return null;
-		case NetMsgTypes.DisconnectREQ:
-			return null;
-		case NetMsgTypes.NewNodeOnNet:
-			return null;
-		case NetMsgTypes.FullNodenameListREQ:
-			return null;
-		case NetMsgTypes.FullNodenameList:
-			return null;
-		case NetMsgTypes.NewSessionREQ:
-			return null;
-		case NetMsgTypes.SessionInfo:
-			return null;
-		case NetMsgTypes.GeometryREQ:
-			return null;
-		case NetMsgTypes.GeometryMANIFEST:
-			return null;
-		case NetMsgTypes.GeometryCHUNK:
-			return null;
+			default:
+				return null;
+			}
 
-		default:
+		} catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
 	}
