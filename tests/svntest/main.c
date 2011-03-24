e9eefde9b4d0aebfe23e44e2c98748c708b556f9
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-03-24 19:25:19 +0000

    Somewhat surprisingly, this approach to subdirectory addition works too.  Files still aren't right

--- tests/svntest/main.c
+++ tests/svntest/main.c
@@ -200,6 +200,7 @@ main(int argc, const char *argv[])
   bu_vls_init(&filepath);
 
   /* will need to use an iterpool in here: http://www.opensubscriber.com/message/users@subversion.tigris.org/8428443.html */
+  data = bu_malloc(sizeof(struct bu_external), "alloc external data struct");
   for (inc=0; inc < RT_DBNHASH; inc++) {
 	  for (dp = dbip->dbi_Head[inc]; dp != RT_DIR_NULL; dp = dp->d_forw) {
 		  if(!BU_STR_EQUAL(dp->d_namep, "_GLOBAL")) {
@@ -207,14 +208,15 @@ main(int argc, const char *argv[])
 			  bu_vls_sprintf(&filedir, "%s/%s", model_name, dp->d_namep);
 			  bu_vls_sprintf(&filepath, "%s/%s/%s", model_name, dp->d_namep, dp->d_namep);
 			  svn_fs_make_dir(txn_root, bu_vls_addr(&filedir), pool);
-			  svn_fs_make_file (txn_root, bu_vls_addr(&filepath), pool);
+/*			  svn_fs_make_file (txn_root, bu_vls_addr(&filepath), pool);
 			  rt_contents = svn_string_ncreate((const char *)data->ext_buf, (apr_size_t)data->ext_nbytes, pool);
 			  rt_data_stream = svn_stream_from_string(rt_contents, pool);
 			  svn_fs_apply_text (&rt_data_stream, txn_root, bu_vls_addr(&filepath), NULL, pool);
-			  svn_stream_close(rt_data_stream);
+			  svn_stream_close(rt_data_stream);*/
 		  }
 	  }
   }
+  bu_free(data, "free external data");
   bu_vls_free(&filedir);
   bu_vls_free(&filepath);
 
