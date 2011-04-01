33eca453f18508ce00edeef2a81ac23c530608bf
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-04-01 20:58:40 +0000

    Add the specific subversion bits as a private header.

---
+++ src/libgvm/gvm_svn.h
@@ -0,0 +1,56 @@
+/*                      G V M _ S V N . H
+ * BRL-CAD
+ *
+ * Copyright (c) 1993-2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file gvm_svn.h
+ *
+ * Subversion specific structures and definitions for GVM.
+ *  
+ */
+
+/**
+ * GVMSVN_ERR(expr)
+ *
+ * We need to handle subversion errors, but don't assume a return
+ */
+#define GVMSVN_ERR(expr)				\
+	do {						\
+		svn_error_t *svn_err = (expr);    	\
+		if (svn_err) {				\
+		   if (SVN_DEBUG)			\
+		      svn_handle_error2(svn_err, stderr, FALSE, "svn: ");  \
+		   ret = svn_err->apr_err;		\
+		   svn_error_clear(svn_err);		\
+		}					\
+	} while (0)
+
+
+/**
+ * SVN Repository info
+ *
+ * This structure holds all state information pertaining to a
+ * subversion geometry repository
+ */
+
+struct geomsvn_info {
+   apr_pool_t *pool;			/* Apache Portable Runtime memory pool */
+   apr_pool_t *commit_pool;		/* Subpool for commits */
+   svn_repos_t *repos;			/* svn_repos_t pointer to repository */
+   svn_revnum_t *curr_rev;		/* Current repository revision (cast to svn_revnum_t to use)*/
+}
+
