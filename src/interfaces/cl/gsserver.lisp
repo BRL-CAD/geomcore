42b537ef39e26f91d7acaa41744d72c1dc02fd3e
Erik Greenwald <erik@brlcad.org>
2011-04-01 21:25:55 +0000

    use read-sequence instead of looping with read-byte

--- src/interfaces/cl/gsserver.lisp
+++ src/interfaces/cl/gsserver.lisp
@@ -15,17 +15,17 @@
   (and (string= user "Guest") (string= pass "Guest")))
 
 (defun send-geom (s reuuid filename)
+  (gsnet:writemsg s (make-instance 'gsnet:geommanifestmsg :manifest (list filename)))
   (with-open-file (stream filename :element-type '(unsigned-byte 8) :if-does-not-exist :error)
-    (loop with a = (make-array (file-length stream) :element-type '(unsigned-byte 8))
-	for i from 0 to (file-length stream)
-	;;; this'd be quicker with read-sequence
-	do (setf (aref a i) (read-byte stream))
-	finally (writemsg s (make-instance 'geomchunkmsg :chunk a)))))
+    (let ((arr (make-array (file-length stream) :element-type '(unsigned-byte 8))))
+      (read-sequence arr stream)
+      (gsnet:writemsg s (make-instance 'gsnet:geomchunkmsg :chunk arr :reuuid reuuid)))))
 
 (defun handle-connection (st)
   (let ((s (make-instance 'gsnet:session :stream st)))
 
     ;;; initial handshane and authentication
+    (setf (gsnet::sessionuuid s) (format '() "~a" (uuid:make-v4-uuid)))
     (setf (gsnet::localnode s) +nodename+)
     (gsnet:writemsg s (make-instance 'gsnet:nodenamemsg :name +nodename+))
     (if (not (gsnet:readmsg s)) (return-from handle-connection '()))
@@ -33,13 +33,13 @@
       (if (equalp (type-of m) 'gsnet:loginmsg)
 	  (if (not (authenticate s (gsnet::username m) (gsnet::password m))) (return-from handle-connection '()))
 	  (return-from handle-connection '())))
-    (gsnet:writemsg s (make-instance 'gsnet::okmsg))
+    (gsnet:writemsg s (make-instance 'gsnet::infomsg :sessionuuid (gsnet::sessionuuid s)))
     
     ;;; main loop
     (loop do
 	 (let ((m (gsnet:readmsg s)))
 	   (cond
-	     ((equalp (type-of m) 'geomreqmsg) (send-geom s (gsnet::uuid m) (gsnet::name m)))
+	     ((equalp (type-of m) 'gsnet:geomreqmsg) (send-geom s (gsnet::uuid m) (gsnet::uri m)))
 	     ((equalp m t) '())
 	     ((equalp m '()) (return-from handle-connection))
 	     (t (format t "Unhandled thing ~a~%" (type-of m))))))))
@@ -50,4 +50,4 @@
 
 
 (defun run (&key (listenhost #(127 0 0 1)) (port 5309))
-  (usocket:socket-server  listenhost port #'handle-connection  '() :element-type 'unsigned-byte)); :multi-threading t :in-new-thread t))
+  (usocket:socket-server listenhost port #'handle-connection  '() :element-type 'unsigned-byte))
