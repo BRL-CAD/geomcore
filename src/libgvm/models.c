822806157402841d5d93f7d69d687db2bdf90be0
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-04-07 16:47:25 +0000

    restructure gvm_get_model to be simpler - don't really need callback.  Start working on gvm_get_objs - this will be tricky since it's essentially a keep without the full database

--- src/libgvm/models.c
+++ src/libgvm/models.c
@@ -34,22 +34,6 @@ int gvm_new_model(struct gvm_info *repo_info, const char *model_name) {
 	return ret;
 }
 
-struct append_callback {
-	struct gvm_info *repo_info;
-	const char *model_name;
-	size_t revnum;
-};
-
-/* Function callback to append repository object to repo_info's objects list 
- * Function type needs to be apr_hash_do_callback_fn_t */
-
-int _append_obj(void *repo_info_in, const void *objname, apr_ssize_t klen, const void *objsvninfo) {
-	struct append_callback *info = (struct append_callback *)repo_info_in;
-	struct repository_objects *new_obj = gvm_get_repo_obj(info->repo_info, info->model_name, objname, info->revnum);
-	new_obj->contents = gvm_get_extern_obj(info->repo_info, info->model_name, objname, info->revnum);
-	gvm_add_to_list(info->repo_info, new_obj);
-}
-
 int gvm_get_model(struct gvm_info *repo_info, const char *model_name, size_t ver_num) {
 	struct geomsvn_info *internal = NULL;
 	internal = (struct geomsvn_info *)repo_info->internal;
@@ -60,15 +44,60 @@ int gvm_get_model(struct gvm_info *repo_info, const char *model_name, size_t ver
 	apr_ssize_t klen;
 	svn_fs_t *fs = svn_repos_fs(internal->repos);
 	svn_fs_root_t *repo_root;
+	struct repository_objects *new_obj;
 	svn_fs_revision_root(&repo_root, fs, (svn_revnum_t)ver_num, subpool);
 	svn_fs_dir_entries(&objects, repo_root, model_name, subpool);
-	struct append_callback callback;
-	callback.repo_info = repo_info;
-	callback.model_name = model_name;
-	callback.revnum = ver_num;
 	for (obj = apr_hash_first(subpool, objects); obj; obj = apr_hash_next(obj)) {
 		apr_hash_this(obj, &key, &klen, NULL);
-		(void)_append_obj((void *)&callback, (const void *)key, klen, NULL);
+		new_obj = gvm_get_repo_obj(repo_info, model_name, (const char *)key, ver_num);
+		new_obj->contents = gvm_get_extern_obj(repo_info, model_name, (const char *)key, ver_num);
+		gvm_add_to_list(repo_info, new_obj);
 	}
 	svn_pool_destroy(subpool);
+}
+
+
+
+int gvm_get_objs(struct gvm_info *repo_info, const char *model_name, const char *obj_name, size_t ver_num, int recursive) {
+	struct geomsvn_info *internal = NULL;
+	internal = (struct geomsvn_info *)repo_info->internal;
+	apr_pool_t *subpool = svn_pool_create(internal->pool);
+	apr_hash_t *objects = apr_hash_make(subpool);
+	apr_hash_t *todo= apr_hash_make(subpool);
+	apr_hash_t *tmp = apr_hash_make(subpool);
+	apr_hash_t *tmp2;
+	apr_hash_index_t *obj;
+	const void *key;
+	const void *val;
+	apr_ssize_t klen;
+	svn_string_t *objname;
+	struct bu_external *contents;
+	/* First, get the requested object */
+	if (recursive) {
+		apr_hash_set(todo, (const void *)obj_name, APR_HASH_KEY_STRING, (const void *)contents);
+	} else {
+		contents = gvm_get_extern_obj(repo_info, model_name, obj_name, ver_num);
+		apr_hash_set(objects, (const void *)obj_name, APR_HASH_KEY_STRING, (const void *)contents);
+	}
+	/* If we aren't recursive, we're done.  If we are, we need to pull the list of
+	 * children of that comb and follow the tree down */
+	/* TODO - in-mem dbi so we can go from external to internal, get the comb list (if any) and
+	 * act on it.  initially put entries found in the todo hash.  if they're present in the 
+	 * objects hash, set to null - otherwise, process them and set to null.  When apr_hash_count
+	 * is 0, we're done.*/
+	while (apr_hash_count(todo)) {
+		for (obj = apr_hash_first(subpool, todo); obj; obj = apr_hash_next(obj)) {
+			apr_hash_this(obj, &key, &klen, &val);
+			if (!apr_hash_get(objects, (const void *)key, APR_HASH_KEY_STRING)) {
+				contents = gvm_get_extern_obj(repo_info, model_name, (const char *)key, ver_num);
+				apr_hash_set(objects, (const void *)key, APR_HASH_KEY_STRING, (const void *)contents);
+				/* contents to internal, get comb list, add to tmp */
+			}
+			apr_hash_set(todo, (const void *)key, APR_HASH_KEY_STRING, NULL);
+		}
+		tmp2 = todo;
+		todo = tmp;
+		tmp = tmp2;
+	}
+
 }
