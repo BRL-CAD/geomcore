9d7c3a258ae2c07027fce5cabec25f1043ceccef
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2011-04-08 22:24:55 +0000

    Hmm - interesting.  Looks like the database needs externs even in cases where the lookup isn't finding anything - this resolves the crash

--- src/libgvm/objects.c
+++ src/libgvm/objects.c
@@ -29,26 +29,22 @@ struct bu_external * gvm_get_extern_obj(struct gvm_info *repo_info, const char *
 	svn_fs_t *fs;
 	struct bu_external *obj_extern;
 	struct geomsvn_info *internal = NULL;
-	if (gvm_object_in_model(repo_info, model_name, obj_name, ver_num)) {
-		internal = (struct geomsvn_info *)repo_info->internal;
-		subpool = svn_pool_create(internal->pool);
-		obj_extern = apr_palloc(internal->objects_pool, sizeof(struct bu_external));
-		fs = svn_repos_fs(internal->repos);
-		svn_fs_revision_root(&repo_root, fs, (svn_revnum_t)ver_num, subpool);
-		svn_string_t *svn_file = svn_string_createf(subpool, "%s/%s/%s", model_name, obj_name, obj_name);
+	internal = (struct geomsvn_info *)repo_info->internal;
+	subpool = svn_pool_create(internal->pool);
+	obj_extern = apr_palloc(internal->objects_pool, sizeof(struct bu_external));
+	fs = svn_repos_fs(internal->repos);
+	svn_fs_revision_root(&repo_root, fs, (svn_revnum_t)ver_num, subpool);
+	svn_string_t *svn_file = svn_string_createf(subpool, "%s/%s/%s", model_name, obj_name, obj_name);
 
-		BU_INIT_EXTERNAL(obj_extern);
-		svn_fs_file_length(&buflen, repo_root, svn_file->data, subpool);
-		obj_extern->ext_nbytes = (size_t)buflen;
-		obj_extern->ext_buf = apr_palloc(internal->objects_pool, obj_extern->ext_nbytes);
-		svn_fs_file_contents(&obj_contents, repo_root, svn_file->data, subpool);
-		svn_stream_read(obj_contents, (char *)obj_extern->ext_buf, (apr_size_t *)&buflen);
-		svn_stream_close(obj_contents);
-		svn_pool_destroy(subpool);
-		return obj_extern;
-	} else {
-		return NULL;
-	}
+	BU_INIT_EXTERNAL(obj_extern);
+	svn_fs_file_length(&buflen, repo_root, svn_file->data, subpool);
+	obj_extern->ext_nbytes = (size_t)buflen;
+	obj_extern->ext_buf = apr_palloc(internal->objects_pool, obj_extern->ext_nbytes);
+	svn_fs_file_contents(&obj_contents, repo_root, svn_file->data, subpool);
+	svn_stream_read(obj_contents, (char *)obj_extern->ext_buf, (apr_size_t *)&buflen);
+	svn_stream_close(obj_contents);
+	svn_pool_destroy(subpool);
+	return obj_extern;
 }
 
 struct repository_objects * gvm_get_repo_obj(struct gvm_info *repo_info, const char *model_name, const char *obj_name, size_t ver_num) {
