3d81c1aecab1a2168d1ec034c9f7fcdbbdc73b77
David Loman <dloman77@gmail.com>
2011-04-11 17:50:18 +0000

    Check in a quick n dirty cmake find module for finding CppUnit

---
+++ CMake/FindCppUnit.cmake
@@ -0,0 +1,54 @@
+#########################################################################
+#
+#	BRL-CAD
+#	
+#	Copyright (c) 1997-2011 United States Government as represented by
+#	the U.S. Army Research Laboratory.
+#	
+#	This library is free software; you can redistribute it and/or
+#	modify it under the terms of the GNU Lesser General Public License
+#	version 2.1 as published by the Free Software Foundation.
+#	
+#	This library is distributed in the hope that it will be useful, but
+#	WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#	Lesser General Public License for more details.
+#	
+#	You should have received a copy of the GNU Lesser General Public
+#	License along with this file; see the file named COPYING for more
+#	information.
+#	
+#########################################################################
+#
+# - Find CppUnit
+# This module finds an installed CppUnit package.
+#
+# It sets the following variables:
+#  CPPUNIT_FOUND       - Set to false, or undefined, if CppUnit isn't found.
+#  CPPUNIT_INCLUDE_DIR - The CppUnit include directory.
+#  CPPUNIT_LIBRARY     - The CppUnit library to link against.
+#	
+#########################################################################
+
+FIND_PATH(CPPUNIT_INCLUDE_DIR cppunit/Test.h)
+FIND_LIBRARY(CPPUNIT_LIBRARY NAMES cppunit)
+
+IF (CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
+   SET(CPPUNIT_FOUND TRUE)
+ENDIF (CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
+
+IF (CPPUNIT_FOUND)
+
+   # show which CppUnit was found only if not quiet
+   IF (NOT CppUnit_FIND_QUIETLY)
+      MESSAGE(STATUS "Found CppUnit: ${CPPUNIT_LIBRARY}")
+   ENDIF (NOT CppUnit_FIND_QUIETLY)
+
+ELSE (CPPUNIT_FOUND)
+
+   # fatal error if CppUnit is required but not found
+   IF (CppUnit_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find CppUnit")
+   ENDIF (CppUnit_FIND_REQUIRED)
+
+ENDIF (CPPUNIT_FOUND)
