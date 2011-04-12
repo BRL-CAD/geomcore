50e70fba28de4bef1b5315bfaabfb60282bd9cad
Erik Greenwald <erik@brlcad.org>
2011-04-12 21:27:05 +0000

    add test function

--- src/interfaces/cl/gvm.lisp
+++ src/interfaces/cl/gvm.lisp
@@ -30,19 +30,25 @@
 #+darwin(pushnew #P"/usr/brlcad/lib/" cffi:*darwin-framework-directories* :test #'equal)
 
 (cffi:define-foreign-library libgvm 
-   (:darwin (:or "libgvm.19.dylib" "libgvm.dylib"))
-   (:unix (:or "libgvm.19.so" "libgvm.so"))
+   (:darwin "libgvm.dylib")
+   (:unix "libgvm.so")
    (:windows "libgvm.dll")
    (t (:default "libgvm")))
 (cffi:use-foreign-library libgvm)
 
 (defctype size_t :unsigned-int)
 
+(defconstant +latest-version+ 0)
+(defconstant +gvm-update-obj+ 1)
+(defconstant +gvm-add-obj+ 2)
+(defconstant +gvm-del-obj+ 3)
+
 (defcfun "gvm_info_init" :void (repo_info :pointer))
 (defcfun "gvm_info_free" :void (repo_info :pointer))
 (defcfun "gvm_info_clear_objects" :void (repo_info :pointer))
 (defcfun "gvm_init_repo" :int (repo_info :pointer) (repo_path :string))
 (defcfun "gvm_open_repo" :int (repo_info :pointer) (repo_path :string))
+(defcfun "gvm_close_repo" :int (repo_info :pointer))
 (defcfun "gvm_object_in_model" :int (repo_info :pointer) (model_name :string) (obj_name :string) (ver_num size_t))
 (defcfun "gvm_get_extern_obj" :pointer (repo_info :pointer) (model_name :string) (obj_name :string) (ver_num size_t))
 (defcfun "gvm_get_repo_obj" :pointer (repo_info :pointer) (model_name :string) (obj_name :string) (ver_num size_t))
@@ -57,3 +63,22 @@
 (defcfun "gvm_export_g_file" :int (repo_info :pointer) (model_name :string) (g_file :string) (ver_num size_t))
 (defcfun "gvm_export_list" :int (repo_info :pointer) (model_name :string) (g_file :string))
 (defcfun "gvm_export_object" :int (repo_info :pointer) (model_name :string) (obj_name :string) (g_file :string) (ver_num size_t) (recursive :int))
+
+(defparameter +repo-file+ "./GS_repository")
+(defun test (file &key (something '()))
+  (time
+   (let ((info (foreign-alloc :pointer :count 3)))
+     (gvm-info-init info)
+     (unless (probe-file +repo-file+) (gvm-init-repo info +repo-file+))
+     (gvm-open-repo info +repo-file+)
+     (gvm-new-model info "test.g")
+     (time (gvm-import-g-file info file))
+     (when something (gvm-commit-g-file info file something))
+     (time
+      (progn
+	(gvm-export-g-file info file "test.g" +latest-version+)
+	(gvm-export-object info file "hull" "tank_obj.g" +latest-version+ 0)
+	(gvm-export-object info file "tank" "tank.g" +latest-version+ 1)))
+;     (gvm-close-repo info)
+     (foreign-free info))))
+
