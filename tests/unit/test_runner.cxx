b8910e4822df6f8f7ba9983b4c1ba2f8e388fd94
David Loman <dloman77@gmail.com>
2011-04-13 12:32:58 +0000

    Update test_runner.cxx with header and footer.  Make console printing a tad prettier.

--- tests/unit/test_runner.cxx
+++ tests/unit/test_runner.cxx
@@ -1,5 +1,30 @@
+/*                 T E S T _ R U N N E R . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file test_runner.cxx
+ *
+ * Runnable portion of the unit test suite.  Build system populates
+ * the list of units to test, this runs the tests.
+ *
+ */
 
-
+#include <iostream>
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/TestResult.h>
@@ -9,6 +34,9 @@
 
 int main (int argc, char* argv[])
 {
+  std::cout << std::endl;
+  std::cout << "***** GeomCore Unit Test Suite. *****\n" << std::endl;
+
     // informs test-listener about testresults
     CPPUNIT_NS :: TestResult testresult;
 
@@ -29,6 +57,17 @@ int main (int argc, char* argv[])
     CPPUNIT_NS :: CompilerOutputter compileroutputter (&collectedresults, std::cerr);
     compileroutputter.write ();
 
+
     // return 0 if tests were successful
+    std::cout << std::endl;
     return collectedresults.wasSuccessful () ? 0 : 1;
 }
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
