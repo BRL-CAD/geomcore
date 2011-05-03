6bbe77ed8b840130531a56d6b8a77b6c30bbae96
David Loman <dloman77@gmail.com>
2011-05-03 15:13:40 +0000

    Add new Job:  MakeAndRouteMsgJob. Will shortly supersede RouteMsgJob

---
+++ include/MakeAndRouteMsgJob.h
@@ -0,0 +1,60 @@
+/*            M A K E A N D R O U T E M S G J O B . H
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file MakeAndRouteMsgJob.h
+ *
+ * Brief description
+ *
+ */
+
+#ifndef __MAKEANDROUTEMSGJOB_H__
+#define __MAKEANDROUTEMSGJOB_H__
+
+#include "NetMsg.h"
+#include "Portal.h"
+#include "AbstractJob.h"
+
+class MakeAndRouteMsgJob : public AbstractJob
+{
+public:
+	MakeAndRouteMsgJob(NetMsg* msg);
+	virtual ~MakeAndRouteMsgJob();
+
+protected:
+	JobResult _doJob();
+
+private:
+	Portal* p;
+
+	/* Disable copy cstr and =operator */
+	MakeAndRouteMsgJob(MakeAndRouteMsgJob const&){};
+	MakeAndRouteMsgJob& operator=(MakeAndRouteMsgJob const&){};
+};
+
+#endif /* __MAKEANDROUTEMSGJOB_H__ */
+
+/*
+ * Local Variables:
+ * tab-width: 8
+ * mode: C
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
---
+++ src/libNet/MakeAndRouteMsgJob.cxx
@@ -0,0 +1,49 @@
+/*          M A K E A N D R O U T E M S G J O B . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file MakeAndRouteMsgJob.cxx
+ *
+ * Brief description
+ *
+ */
+
+#include "MakeAndRouteMsgJob.h"
+#include "NetMsgRouter.h"
+
+MakeAndRouteMsgJob::MakeAndRouteMsgJob(Portal* p) :
+	p(p)
+{}
+
+MakeAndRouteMsgJob::~MakeAndRouteMsgJob() {}
+
+JobResult MakeAndRouteMsgJob::_doJob()
+{
+  Portal::tryToBuildNetMsgs(this->p);
+}
+
+
+/*
+ * Local Variables:
+ * mode: C
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * c-file-style: "stroustrup"
+ * End:
+ * ex: shiftwidth=4 tabstop=8
+ */
