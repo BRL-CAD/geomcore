74802ede3644b0f2ac27754f9041ad76f8882960
Erik Greenwald <erik@brlcad.org>
2011-05-04 20:26:31 +0000

    reflect changes in GSNet protocol (elimination of magic, swapping of type and length)

--- src/interfaces/cl/gsnet.lisp
+++ src/interfaces/cl/gsnet.lisp
@@ -12,7 +12,6 @@
 (in-package :gsnet)
 
 (defparameter +nodename+ "Geist")
-(defconstant +magic+ #x41fe5309)
 
 ;;; message types
 (defconstant +gsrualive+ #x0042) ; Test if server is up
@@ -75,45 +74,43 @@
 
 ;;; snarf data off the line and return an instance of the right kind of class
 (defun readmsg (s)
-  (if (readmagic (strm s))
-      (let ((length (readuint32 (strm s)))
-	    (type (readuint16 (strm s)))
-	    (uuid (readgsstring (strm s)))
-	    (reuuid (when (= (read-byte (strm s)) 1) (readuuid (strm s)) '())))
-	(declare (ignore length))
-	(declare (ignore uuid))
-	(declare (ignore reuuid))
-	(cond
-	  ((= type +gsrnnset+)	(setf (remotenode s) (readgsstring (strm s))) t)
-	  ((= type +gsdr+)	(writemsg s (make-instance 'logoutmsg)) '())
-	  ((= type +gspong+)	(make-instance 'pongmsg :tv (readuint64 (strm s))))
-	  ((= type +gsping+)	(writemsg s (make-instance 'pongmsg :tv (readuint64 (strm s)))) t) ; automatically respond to ping requests
-	  ((= type +gsinfo+)	(setf (sessionuuid s) (readgsstring (strm s))) t)
-	  ((= type +gsfail+)	(make-instance 'failmsg))
-	  ((= type +gsok+)	(make-instance 'okmsg))
-	  ((= type +gsrualive+)	(writemsg s (make-instance 'imalivemsg)) t) ; automatically respond to rualive 
-	  ((= type +gsimalive+)	(make-instance 'imalivemsg))
-	  ((= type +gsgr+)	(make-instance 'grmsg :uri (readgsstring (strm s))))
-	  ((= type +gsgbr+)	(make-instance 'gbrmsg :uri (readgsstring (strm s))))
-	  ((= type +gsgm+)	(make-instance 'gmmsg :manifest (loop for i from 1 to (readuint32 (strm s)) collect (readgsstring (strm s)))))
-	  ((= type +gsgc+)	(make-instance 'gcmsg :chunk 
-					  (let ((arr (make-array (+	(readuint32 (strm s)) 1) :element-type '(unsigned-byte 8))))
-						(read-sequence arr (strm s))
-						arr)))
-	  ((= type +gsls+)	(make-instance 'lsmsg :uri (readgsstring (strm s))))
-	  ((= type +gslsr+)	(make-instance 'lsrmsg :manifest (loop for i from 1 to (readuint32 (strm s)) collect (readgsstring (strm s)))))
-	  ((= type +gsnsr+)	(make-instance 'nsrmsg :username (readgsstring (strm s)) :password (readgsstring (strm s))))
-	  (t (format t "Unknown type! ~a ~x~%" (type-of type) type))))
-      '()))
+  (let ((type (readuint16 (strm s)))
+	(length (readuint32 (strm s)))
+	(uuid (readgsstring (strm s)))
+	(reuuid (when (= (read-byte (strm s)) 1) (readuuid (strm s)) '())))
+    (declare (ignore length))
+    (declare (ignore uuid))
+    (declare (ignore reuuid))
+    (cond
+      ((= type +gsrnnset+)	(setf (remotenode s) (readgsstring (strm s))) t)
+      ((= type +gsdr+)	(writemsg s (make-instance 'logoutmsg)) '())
+      ((= type +gspong+)	(make-instance 'pongmsg :tv (readuint64 (strm s))))
+      ((= type +gsping+)	(writemsg s (make-instance 'pongmsg :tv (readuint64 (strm s)))) t) ; automatically respond to ping requests
+      ((= type +gsinfo+)	(setf (sessionuuid s) (readgsstring (strm s))) t)
+      ((= type +gsfail+)	(make-instance 'failmsg))
+      ((= type +gsok+)	(make-instance 'okmsg))
+      ((= type +gsrualive+)	(writemsg s (make-instance 'imalivemsg)) t) ; automatically respond to rualive 
+      ((= type +gsimalive+)	(make-instance 'imalivemsg))
+      ((= type +gsgr+)	(make-instance 'grmsg :uri (readgsstring (strm s))))
+      ((= type +gsgbr+)	(make-instance 'gbrmsg :uri (readgsstring (strm s))))
+      ((= type +gsgm+)	(make-instance 'gmmsg :manifest (loop for i from 1 to (readuint32 (strm s)) collect (readgsstring (strm s)))))
+      ((= type +gsgc+)	(make-instance 'gcmsg :chunk 
+				       (let ((arr (make-array (+	(readuint32 (strm s)) 1) :element-type '(unsigned-byte 8))))
+					 (read-sequence arr (strm s))
+					 arr)))
+      ((= type +gsls+)	(make-instance 'lsmsg :uri (readgsstring (strm s))))
+      ((= type +gslsr+)	(make-instance 'lsrmsg :manifest (loop for i from 1 to (readuint32 (strm s)) collect (readgsstring (strm s)))))
+      ((= type +gsnsr+)	(make-instance 'nsrmsg :username (readgsstring (strm s)) :password (readgsstring (strm s))))
+      (t (format t "Unknown type! ~a ~x~%" (type-of type) type))))
+  '())
 
 (defgeneric writemsg (session message) (:documentation "Send the message to the socket stream"))
 
 ;;; common to all messages
 (defmethod writemsg :before (s (m message)) (setf (len m) (+ (len m) 7 (length (uuid m)) (if (reuuid m) (+ (length (reuuid m)) 4) 0) )))
 (defmethod writemsg (s (m message))
-  (writeuint32 (strm s) +magic+) 
-  (writeuint32 (strm s) (len m)) 
   (writeuint16 (strm s) (msgtype m)) 
+  (writeuint32 (strm s) (len m)) 
   (writegsstring (strm s) (uuid m))
   (if (reuuid m)
       (progn (write-byte 1 (strm s)) (writegsstring (strm s) (reuuid m)))
