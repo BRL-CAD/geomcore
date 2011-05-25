8f43cbd9815466107dc700bd1b98e414222d5d62
David Loman <dloman77@gmail.com>
2011-05-25 11:06:40 +0000

    Rough in skeleton for BrlcadDb class.  Will contain all fns necessary for loading, reading, writing and saving a .g file.

---
+++ src/GS/BrlcadDb.cxx
@@ -0,0 +1,46 @@
+/*                    B R L C A D D B . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file BrlcadDb.cxx
+ *
+ */
+
+#include "BrlcadDb.h"
+#include "db.h"
+#include "raytrace.h"
+
+BrlcadDb::BrlcadDb()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+BrlcadDb::~BrlcadDb()
+{
+  // TODO Auto-generated destructor stub
+}
+
+// Local Variables:
+// tab-width: 8
+// mode: C++
+// c-basic-offset: 4
+// indent-tabs-mode: t
+// c-file-style: "stroustrup"
+// End:
+// ex: shiftwidth=4 tabstop=8
