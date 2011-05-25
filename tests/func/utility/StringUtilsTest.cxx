d33e8591c72ba3f2282812b23279ea420e265807
David Loman <dloman77@gmail.com>
2011-05-25 14:04:13 +0000

    Implement a stringUtils functional test.

---
+++ tests/func/utility/StringUtilsTest.cxx
@@ -0,0 +1,41 @@
+/*       S T R I N G U T I L S T E S T . C X X
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file StringUtils.cxx
+ *
+ */
+
+#include "StringUtils.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+
+  return 0;
+}
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 8 ***
+// c-basic-offset: 4 ***
+// indent-tabs-mode: t ***
+// End: ***
+// ex: shiftwidth=4 tabstop=8
