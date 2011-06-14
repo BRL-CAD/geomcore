56380a28dadd86764004b6523e4323fea677a48f
David Loman <dloman77@gmail.com>
2011-06-14 19:07:10 +0000

    Add a response handler for NetMsg that should envoke a response from server.

---
+++ src/interfaces/java/src/org/brlcad/geometryservice/net/GSNetMsgFutureResponse.java
@@ -0,0 +1,71 @@
+/*
+ * BRL-CAD
+ *
+ * Copyright (c) 2011 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
+ */
+/** @file GSNetMsgFutureResponse.java
+ * 
+ */
+package org.brlcad.geometryservice.net;
+
+import java.util.UUID;
+
+import org.brlcad.geometryservice.net.msg.AbstractNetMsg;
+
+public class GSNetMsgFutureResponse {
+	private final UUID originalMsgID;
+	private AbstractNetMsg responseMsg = null;
+
+	/**
+	 * @param originalMsgID
+	 */
+	public GSNetMsgFutureResponse(UUID originalMsgID) {
+		super();
+		this.originalMsgID = originalMsgID;
+	}
+
+	public boolean hasResponse() {
+		return (this.responseMsg != null);
+	}
+
+	/**
+	 * Blocks until a response to a message arrives.
+	 * 
+	 * @return If null, then error occurred. Otherwise, the response Msg is
+	 *         returned.
+	 */
+	public AbstractNetMsg blockUntilResponse() {
+		while (this.responseMsg == null) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		return this.getResponseMsg();
+	}
+	protected void setResponseMsg(AbstractNetMsg response) {
+		this.responseMsg = response;
+	}
+	public UUID getOriginalMsgID() {
+		return originalMsgID;
+	}
+	public AbstractNetMsg getResponseMsg() {
+		return responseMsg;
+	}
+}
