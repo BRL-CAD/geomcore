c65d4323278b830ca0fdfc9f855e9cd0f9e62a14
David Loman <dloman77@gmail.com>
2011-06-14 15:15:06 +0000

    Update NetMsgTypes from C++ code.

--- src/interfaces/java/src/org/brlcad/geometryservice/net/msg/NetMsgTypes.java
+++ src/interfaces/java/src/org/brlcad/geometryservice/net/msg/NetMsgTypes.java
@@ -29,7 +29,7 @@ public final class NetMsgTypes {
 	public static final short Failure = 0x0050;
 	public static final short Success = 0x0051;
 	public static final short Ping = 0x0060;
-	public static final short Pong = 0x0061;
+	public static final short Pong = 0x0062;
 	public static final short RemNodeNameSET = 0x0100;
 	public static final short DisconnectREQ = 0x0150;
 	public static final short NewNodeOnNet = 0x0200;
@@ -38,6 +38,8 @@ public final class NetMsgTypes {
 	public static final short NewSessionREQ = 0x0300;
 	public static final short SessionInfo = 0x0305;
 	public static final short GeometryREQ = 0x0400;
+	public static final short DirListREQ = 0x0402;
+	public static final short DirListRES = 0x0403;
 	public static final short GeometryMANIFEST = 0x0405;
 	public static final short GeometryCHUNK = 0x0410;
 
